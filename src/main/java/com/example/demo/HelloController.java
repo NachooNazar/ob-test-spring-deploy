a490475754bd9a185e870d4fe97d3e6172c96dc0
NachooNazar <94497872+NachooNazar@users.noreply.github.com>
2022-08-17 16:21:39 -0300

    Deploy heroku config

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${app.message}")
+    private String message;
+
+    @Value("${app.varexample}")
+    private String varexample;
+
+    @GetMapping("/hello")
+    public String HelloWorld(){
+        return "Hello World! " + varexample;
+    }
+}
